71ce1dc6a6d3ed34d00552d886ec0b9b0543d681
HydreigonGuy <ad.thibault18@gmail.com>
2024-12-21 22:23:14 +0100

    added user to migration file

--- migrations/0001_tabs.sql
+++ migrations/0001_tabs.sql
@@ -1,5 +1,12 @@
+create table users (
+    id int PRIMARY KEY,
+    username varchar not null,
+    password varchar not null
+);
+
 create table tab (
     id SERIAL PRIMARY KEY,
     title varchar not null,
-    tab INTEGER[6][] NULL
-)
+    tab INTEGER[6][] NULL,
+    UserID int REFERENCES users(id) ON DELETE CASCADE
+);
