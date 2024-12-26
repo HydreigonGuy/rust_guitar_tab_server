6e3ba3e07872783deae41bfc3d73175b01c1b7c8
HydreigonGuy <ad.thibault18@gmail.com>
2024-12-26 21:14:10 +0100

    added random string generating for a user token

--- src/db/db_handling.rs
+++ src/db/db_handling.rs
@@ -2,6 +2,7 @@
 use sqlx::Row;
 use std::error::Error;
 use pwhash::bcrypt;
+use rand::{distributions::Alphanumeric, Rng};
 
 pub async fn check_if_username_is_taken(db_pool: sqlx::PgPool, username: &str) -> Result<bool, Box<dyn Error>> {
     let q = format!("SELECT id FROM users WHERE username = '{}'", username.to_string());
@@ -28,4 +29,13 @@ pub async fn check_login_auth(db_pool: sqlx::PgPool, username: &str, password: &
         return Ok(true);
     }
     return Ok(false);
+}
+
+pub async fn create_token_for_user(db_pool: sqlx::PgPool, user_id: u32) -> Result<String, Box<dyn Error>> {
+    let s: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(64)
+        .map(char::from)
+        .collect();
+    Ok(s)
 }
