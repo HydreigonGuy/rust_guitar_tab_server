580d5a0a0a5a64ad8b9f9107a1d54be56b3ec465
HydreigonGuy <ad.thibault18@gmail.com>
2024-12-28 21:27:08 +0100

    added a fix to a bug which hid private tabs

--- js/list.js
+++ js/list.js
@@ -24,7 +24,7 @@ async function get_tabs() {
     .then(data => {
         const tabs = JSON.parse(data).res;
 
-        document.getElementById("list_container").innerHTML = "";
+        document.getElementById("public_list_container").innerHTML = "";
         for (i = 0; i < tabs.length; i++) {
             document.getElementById("public_list_container").innerHTML = document.getElementById("public_list_container").innerHTML + '<a class="tab_link" href="/list/' + tabs[i].id + '">' + tabs[i].title + "</a>";
         }
